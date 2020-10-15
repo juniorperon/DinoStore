a44ee7b7742ac8d953ea57dc48dbface6e531078
Eliandro Freitas <eliandroelias@hotmail.com>
2020-10-14 21:02:25 -0300

    TELA DE CARREGAMENTO

--- src/lojadino/LojaDino.java
+++ src/lojadino/LojaDino.java
@@ -7,6 +7,7 @@ public class LojaDino {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        new Carregamento().setVisible(true);
         // TODO code application logic here
     }
     
