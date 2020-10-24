6781e17576a773daf932113131e46c5e61a9f941
Ailton Peron Junior <junior.peron@hotmail.com>
2020-10-24 16:16:15 -0300

    cadastro de clientes e dinossauros

--- src/lojadino/LojaDino.java
+++ src/lojadino/LojaDino.java
@@ -1,20 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package lojadino;
 
-/**
- *
- * @author Paulo
- */
 public class LojaDino {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        new Carregamento().setVisible(true); // Iniciar aplicativo, rodar tela de carregamento
         // TODO code application logic here
     }
     
